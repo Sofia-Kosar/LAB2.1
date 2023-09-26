004849385762524b9fa1d24d02220a88c8e45a6f
Sofia-kosar <Sofiia.Kosar.RI.2023@lpnu.ua>
2023-09-26 13:38:57 +0300

    first branch2 commit

--- LAB2.1/lab2.1.cpp
+++ LAB2.1/lab2.1.cpp
@@ -10,7 +10,7 @@ int main()
 {
 	double a; // вхідний параметр
 	double Pi = 4 * atan(1.); // число пі
-	double z1; // результат обчислення 1-го виразу
+	// double z1; // результат обчислення 1-го виразу
 	double z2; // результат обчислення 2-го виразу
 
 	
@@ -18,12 +18,12 @@ int main()
 
 	
 	cout << "a = "; cin >> a;
-	z1 = (sin((Pi/2)+3*a))/(1-sin(3*a+Pi));
+	// z1 = (sin((Pi/2)+3*a))/(1-sin(3*a+Pi));
 	
 
 	z2 = cos((5 * Pi / 4) + 3 * a  / 2) / sin((5 * Pi / 4) + 3 * a / 2);
 	cout << endl;
-	cout << "z1 = " << z1 << endl;
+	// cout << "z1 = " << z1 << endl;
 	cout << "z2 = " << z2 << endl;
 	cin.get();
 	return 0;
